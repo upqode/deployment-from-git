8471a3c7b1b799cb39f2ea8f727e0cff589a7c2f
i3y3ik <i3y3ik@gmail.com>
2016-12-25 20:07:30 +0200

    +backups list with repository

--- controllers/BackupController.php
+++ controllers/BackupController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\components\BaseController;
+use app\models\Backups;
 use yii\filters\AccessControl;
 
 class BackupController extends BaseController
@@ -25,9 +26,26 @@ class BackupController extends BaseController
         ];
     }
 
+    /**
+     * Show repository list with backup info
+     *
+     * @return string
+     */
     public function actionIndex()
     {
-        return $this->render('index');
+        $backups = Backups::find()
+            ->select([
+                'DISTINCT(backups.repository_id) as repository_id',
+                'COUNT(backups.repository_id) as count',
+                'repositories.name as repository_name',
+                'MAX(backups.time) as time',
+            ])
+            ->leftJoin('repositories', 'repositories.id = backups.repository_id')
+            ->groupBy('repository_id')
+            ->asArray()
+            ->all();
+
+        return $this->render('index', ['backups' => $backups]);
     }
 
 }
